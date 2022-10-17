028e6fdcf6d6897823c994a1a3f401da4b110a00
Elphill <Elphill@DESKTOP-QEBQ8BA>
2022-10-17 12:37:52 +0530

    My commit changes

---
+++ src/test/java/Features/login.feature
@@ -0,0 +1,14 @@
+Feature: IssueWire login feature
+
+Scenario: IssueWire login Test scenario
+
+Given user is already on front page
+When title of front page is IssueWire
+Then click to the login button
+Then user enters user name
+Then user enters password
+And user clicks on login button
+Then click to the add credit button
+And new window will be open
+Then dropdown will do select
+Then return to the home page
---
+++ src/test/java/Test_Runner/Runner.java
@@ -0,0 +1,21 @@
+package Test_Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class) @CucumberOptions( 
+	    features ="C:\\Users\\Elphill\\eclipse-workspace\\IssueWire_BDD\\src\\test\\java\\Features\\login.feature", 
+	    glue = {"stepDefinitions"},
+	    
+	    //format= {"pretty","html:target/cucumber"}
+ plugin= {"pretty","html:target/cucumber-reports"},
+ monochrome=true 
+)
+
+public class Runner {
+	
+	
+
+}
